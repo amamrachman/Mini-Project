15f73501091e10c1a5d07371c20a97ec2e4ae0ea
amamrachmanto <amam11rachmanto@gmail.com>
2026-02-02 09:27:51 +0700

    perbaikan cors

--- Backend/bootstrap/app.php
+++ Backend/bootstrap/app.php
@@ -4,21 +4,17 @@ use Illuminate\Foundation\Application;
 use Illuminate\Http\Middleware\HandleCors;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
-        apiPrefix: 'api',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-         $middleware->api(prepend: [
-            HandleCors::class,
-            EnsureFrontendRequestsAreStateful::class,
-        ]);
+        $middleware->append(HandleCors::class);
+        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
