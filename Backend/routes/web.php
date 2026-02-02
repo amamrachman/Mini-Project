1eba7f9b9ae1931c19cd8f3e13c1b77bed773b09
amamrachmanto <amam11rachmanto@gmail.com>
2026-02-02 09:28:47 +0700

    mebuat route untuk auth

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -1,27 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
 
-Route::post('/login', function (Request $request) {
-    if (!Auth::attempt($request->only('email', 'password'))) {
-        return response()->json(['message' => 'login gagal'], 401);
-    }
-
-    $request->session()->put('TEST_SESSION', 'OK');
-    $request->session()->regenerate();
-
-    return response()->json([
-        'message' => 'login sukses',
-        'session_id' => session()->getId(),
-    ]);
-});
-
-Route::get('/cek-session', function () {
-    return response()->json([
-        'session' => session()->all(),
-        'user' => auth()->user(),
-    ]);
-});
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
