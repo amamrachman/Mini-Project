117091f5e28880a82b43bb8779960d5e5435bddb
amamrachmanto <amam11rachmanto@gmail.com>
2026-02-02 09:27:24 +0700

    membuat model user

--- Backend/app/Models/User.php
+++ Backend/app/Models/User.php
@@ -20,14 +20,11 @@ class User extends Authenticatable
 
     protected $hidden = [
         'password',
-        'remember_token',
+        // 'remember_token',
     ];
 
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+    protected $casts = [
+        // 'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
 }
