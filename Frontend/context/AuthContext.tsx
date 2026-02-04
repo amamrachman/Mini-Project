6e601cf5963bc5de4d7ee43db06005bfeaf82f4a
amamrachmanto <amam11rachmanto@gmail.com>
2026-02-04 12:00:50 +0700

    membuat context agar mengetahui user yang login

---
+++ Frontend/context/AuthContext.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { syncTodos } from "@/lib/api";
+import { authLogin, getCurrentUser } from "@/lib/auth";
+import { LoginPayload } from "@/types/Auth";
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+type AuthContextType = {
+  is_login: boolean;
+  login: (payload: { email: string; password: string }) => void;
+  checkAuth: () => void;
+};
+
+export const AuthContext = createContext<AuthContextType | null>(null);
+
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [is_login, setIs_login] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const checkAuth = async () => {
+    try {
+      await getCurrentUser();
+      setIs_login(true);
+    } catch (err) {
+      setIs_login(false);
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  const login = async (payload: LoginPayload) => {
+    await authLogin(payload);
+
+    const localTodosStr = localStorage.getItem("guestTodos");
+    const localTodos = localTodosStr ? JSON.parse(localTodosStr) : [];
+
+    if (localTodos.length > 0) {
+      await syncTodos(localTodos);
+      localStorage.removeItem("guestTodos");
+    }
+
+    setIs_login(true);
+  };
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        Loading...
+      </div>
+    );
+  }
+
+  return (
+    <AuthContext.Provider value={{ is_login, login, checkAuth }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuthProvider = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("Gunakan Dalam AuthProvider");
+  }
+
+  return context;
+};
+
+export default AuthProvider;
